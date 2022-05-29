2209d121055f0b3af776f811738e0281d8adc9da
IvaanOne <ivaanone17@gmail.com>
2022-05-29 23:40:08 +0200

    Pantalla Carrera Diseño hecho

--- js/main.js
+++ js/main.js
@@ -31,24 +31,23 @@ let eleccion1 = (nBoat) => {
     if(seleccion1 == ""){
         seleccion1 = allBoats[nBoat];
         let primerBoat = document.getElementById(nBoat);
-        let dataBoat = document.getElementById("data" + 1);
+        let dataBoats = document.getElementById("data" + 1);
         primerBoat.onclick = "";
-        //Bloqueamos el otro barco
+        //Bloqueamos el barco
         primerBoat.classList.add("seleccionado");
-        dataBoat.innerHTML = `${seleccion1.name}`;
-        
-        
-    } else if( team2 == ""){
-        seleccion1 = allBoats[nBoat];
+        dataBoats.innerHTML = `${seleccion1.name}`;
+
+    }else if(seleccion2 == ""){
+        seleccion2 = allBoats[nBoat];
         let primerBoat = document.getElementById(nBoat);
-        let dataBoat = document.getElementById("data" + 2);
+        let dataBoats = document.getElementById("data" + 2);
         primerBoat.onclick = "";
         //Bloqueamos el otro barco
         primerBoat.classList.add("seleccionado");
-        dataBoat.innerHTML = `${seleccion2.name}`;        
+        dataBoats.innerHTML = `${seleccion2.name}`;        
 
         setTimeout(() => {
-            changeScreen(3);
+            cambioPantalla(3);
             displayGame();
         }, 2500);
     }
@@ -63,7 +62,7 @@ class boat {
         this.id = id;
         this.motor = "4t";
         this.velocidad = 0;
-        this.luck = functions.random(1,20);
+        this.luck = aleatorio.random(1,20);
     }
 
     correr() {
@@ -76,8 +75,8 @@ class boat {
 
 // Instanciamos los Barcos --<<<<<<
 
-let boat1 = new boat ("Red", "1", "4t", 0, 1);
-let boat2 = new boat ("Blue", "2", "4t", 0, 1);
+let boat1 = new boat ("RED FLAG", "1", "4t", 0, 1);
+let boat2 = new boat ("BLUE PEARL", "2", "4t", 0, 1);
 
 
 //Diccionario --<<<<<
